8a28d2558987e0039e0abba88ecbd1e97f30e6c7
Adam77Root <adam77root@gmail.com>
2012-07-09 10:25:42 +0200

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,141 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+# inherit from the proprietary version
+-include vendor/samsung/galaxyr/BoardConfigVendor.mk
+
+# CPU
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+# DO NOT change the following line to vfpv3 as it is not really supported on our device!
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+#TARGET_HAVE_TEGRA_ERRATA_657451 := true
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := ap20
+TARGET_BOARD_PLATFORM_GPU := tegra
+TARGET_BOOTLOADER_BOARD_NAME := n1
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_NAND_PAGE_SIZE := 4096
+BOARD_NAND_SPARE_SIZE := 128
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_CMDLINE := mem=511M@0M secmem=1M@511M mem=512M@512M vmalloc=256M fota_boot=false lpm_boot=0 tegra_fbmem=800K@0x18012000 video=tegrafb console=ram usbcore.old_scheme_first=1 lp0_vec=8192@0x1819E000 emmc_checksum_done=true emmc_checksum_pass=true tegraboot=sdmmc gpt
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 5242880
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 629145600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+TARGET_PREBUILT_KERNEL := device/samsung/galaxyr/kernel
+
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# RIL
+BOARD_USES_LIBSECRIL_STUB := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+TARGET_PROVIDES_LIBAUDIO := false # We must build it.
+
+# Camera
+#USE_CAMERA_STUB := true
+BOARD_VENDOR_USE_NV_CAMERA := true
+BOARD_CAMERA_LIBRARIES := libcamera
+BOARD_SECOND_CAMERA_DEVICE := true
+
+# Graphics
+BOARD_EGL_CFG := device/samsung/galaxyr/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+#BOARD_FORCE_STATIC_A2DP := true
+
+# Mobile data
+BOARD_MOBILEDATA_INTERFACE_NAME := "rmnet0"
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := si4709
+
+# Akmd
+BOARD_VENDOR_USE_AKMD := akm8973
+BUILD_AKMD := true
+
+# Vibrator
+#BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/c1-common/vibrator/tspdrv.c
+
+# GPS
+BOARD_USES_GPSWRAPPER := true
+
+# Wifi related defines
+WPA_BUILD_SUPPLICANT        := true
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WLAN_DEVICE           := bcm4330
+WIFI_DRIVER_MODULE_PATH     := "/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/bcm4330_sta.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/bcm4330_aps.bin"
+WIFI_DRIVER_FW_MFG_PATH     := "/system/etc/wifi/bcm4330_mfg.bin"
+#WIFI_FIRMWARE_LOADER       := "wlandutservice"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+
+# Releasetools
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxyr/releasetools/galaxyr_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxyr/releasetools/galaxyr_img_from_target_files
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := galaxyr,GT-I9103
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 12
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun%d/file"
+
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p1
+
+# Recovery
+TARGET_RECOVERY_INITRC := ./device/samsung/galaxyr/recovery.rc
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxyr/recovery/recovery_keys.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxyr/recovery/graphics.c
+
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
---
+++ galaxyr.mk
@@ -0,0 +1,234 @@
+#
+# This file is the build configuration for a full Android
+# build for sapphire hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+
+## Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+
+#DEVICE_PACKAGE_OVERLAYS := device/samsung/cooper/overlay
+
+# proprietary side of the device
+#$(call inherit-product-if-exists, vendor/samsung/galaxyr/device-vendor.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := galaxyr
+PRODUCT_DEVICE := galaxyr
+PRODUCT_MODEL := GT-I9103
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/samsung/galaxyr/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    HoloSpiralWallpaper \
+    Gallery3d \
+    SpareParts \
+    Development \
+    Term \
+    libOmxCore \
+    libOmxVidEnc \
+    sec_touchscreen.kcm \
+    dexpreopt \
+    com.android.future.usb.accessory \
+    galaxyrSettings \
+    SamsungServiceMode \
+    Torch \
+    Galaxy4 \
+    NoiseField \
+    PhaseBeam \
+    librs_jni
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    static_busybox \
+    make_ext4fs \
+    setup_fs
+
+PRODUCT_PACKAGES += \
+    sensors.n1 \
+    sensors.tegra \
+    lights.tegra \
+    gps.n1 \
+    gralloc.tegra \
+    overlay.tegra
+
+# Set true if you want .odex files
+DISABLE_DEXPREOPT := false
+
+# INIT-scripts
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/init.n1.rc:root/init.n1.rc \
+    device/samsung/galaxyr/ueventd.n1.rc:root/ueventd.n1.rc
+
+# Prebuilt modules
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/prebuilt/dhd.ko:root/lib/modules/dhd.ko \
+    device/samsung/galaxyr/prebuilt/bthid.ko:root/lib/modules/bthid.ko \
+    device/samsung/galaxyr/prebuilt/scsi_wait_scan.ko:root/lib/modules/scsi_wait_scan.ko \
+    device/samsung/galaxyr/prebuilt/Si4709_driver.ko:root/lib/modules/Si4709_driver.ko
+
+# Vold and Storage
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/configs/vold.fstab:system/etc/vold.fstab
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/configs/wifi.conf:system/etc/wifi/wifi.conf \
+    device/samsung/galaxyr/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# GPS
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/configs/gps.conf:system/etc/gps.conf \
+    device/samsung/galaxyr/configs/sirfgps.conf:system/etc/sirfgps.conf
+
+# Media
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxyr/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/galaxyr/usr/keylayout/Broadcom_Bluetooth_HID.kl:system/usr/keylayout/Broadcom_Bluetooth_HID.kl \
+    device/samsung/galaxyr/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/galaxyr/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/galaxyr/usr/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl \
+    device/samsung/galaxyr/usr/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
+
+# Keychars
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/usr/keychars/Broadcom_Bluetooth_HID.kcm.bin:system/usr/keychars/Broadcom_Bluetooth_HID.kcm.bin \
+    device/samsung/galaxyr/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+    device/samsung/galaxyr/usr/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/samsung/galaxyr/usr/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
+    device/samsung/galaxyr/usr/keychars/sec_key.kcm.bin:system/usr/keychars/sec_key.kcm.bin \
+    device/samsung/galaxyr/usr/keychars/sec_touchscreen.kcm.bin:system/usr/keychars/sec_touchscreen.kcm.bin
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+
+# Feature live wallpaper
+PRODUCT_COPY_FILES += \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# Pick up overlay for features that depend on non-open-source files
+#DEVICE_PACKAGE_OVERLAYS := vendor/samsung/galaxyr/overlay
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.3g.prefix=1 \
+    ro.ril.enable.a53=1 \
+    ro.ril.enable.dtm=1 \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.gprsclass=12 \
+    ro.ril.hep=1 \
+    ro.ril.hsdpa.category=8 \
+    ro.ril.hsupa.category=5 \
+    ro.ril.hsxpa=2 \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs="-d /dev/ttys0"
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.opengles.version=131072
+
+# These are the hardware-specific settings that are stored in system properties.
+# Note that the only such settings should be the ones that are too low-level to
+# be reachable from resources or other mechanisms.
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=30 \
+    ro.board.platform=tegra \
+    ro.sf.lcd_density=240 \
+    ro.telephony.ril_class=samsung \
+    ro.telephony.sends_barcount=1 \
+    ro.com.android.dataroaming=false \
+    mobiledata.interfaces=eth0,rmnet0,rmnet1,rmnet2 \
+    dalvik.vm.heapsize=64m \
+    persist.service.usb.setting=0 \
+    dev.sfbootcomplete=0 \
+    persist.sys.vold.switchexternal=1
+
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
+
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.android.dateformat=yyyy-MM-dd \
+    ro.setupwizard.enable_bypass=1 \
+    ro.media.dec.jpeg.memcap=20000000 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.heapsize=64m \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.dexopt-data-only=1 \
+    hwui.render_dirty_regions=false \
+    ro.compcache.default=0 \
+    media.stagefright.enable-player=false \
+    media.stagefright.enable-meta=false \
+    media.stagefright.enable-scan=false \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-rtsp=false \
+    ro.tether.denied=false \
+    ro.flash.resolution=1080
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mass_storage
+
+PRODUCT_LOCALES += hdpi
+
+# See comment at the top of this file. This is where the other
+# half of the device-specific product definition file takes care
+# of the aspects that require proprietary drivers that aren't
+# commonly available
+
+$(call inherit-product-if-exists, vendor/samsung/galaxyr/galaxyr-vendor-blobs.mk)
