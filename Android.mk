de334edc61cde20ddbcad61d66e6519f76861e11
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-12 06:45:38 +0200

    prevent conflicts with i9103 repo
    This is required to prevent conflicts in module names (like libaudio)
    
    Change-Id: I8afd9b0ef4f7e4f9b41175457e69ca50cc970239

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(PRODUCT_NAME),cyanogen_galaxyr)
+
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(all-subdir-makefiles)
+
+endif
