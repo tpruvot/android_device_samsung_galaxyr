fe284fb2b06064e65a326d6402c3f7240b46cb7e
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-12 08:42:12 +0200

    fix the makefile conflict protection
    Change-Id: I1e03c5b625fa9a292dbf70f33da0406cdc2d5832

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(PRODUCT_NAME),cyanogen_galaxyr)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),n1)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
