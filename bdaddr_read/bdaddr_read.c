70fbeabb0f4513d39976e91308034ac1eb42a5de
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-08-02 21:46:42 +0200

    bdaddr: add support for persist.service.brcm.bt.mac
    without that, the serial of the device is used as mac address
    
    also write a file with the good format /efs/imei/btaddr.mac
    
    Change-Id: Ibb0b27b2fd7d8f47c82eb43fb1286999f7f60c1d

--- bdaddr_read/bdaddr_read.c
+++ bdaddr_read/bdaddr_read.c
@@ -1,19 +1,21 @@
-#include <fcntl.h>
-#include <string.h>
-#include <cutils/properties.h>
-#include <cutils/log.h>
-
-#define LOG_TAG "bdaddr"
-#define SAMSUNG_BDADDR_PATH "/efs/imei/bt.txt"
-#define BDADDR_PATH "/data/bdaddr"
-
 /* Read bluetooth MAC from SAMSUNG_BDADDR_PATH (different format),
  * write it to BDADDR_PATH, and set ro.bt.bdaddr_path to BDADDR_PATH
  *
  * Adapted from bdaddr_read.c of thunderg
  */
 
-int main() {
+#define LOG_TAG "bdaddr"
+
+#include <fcntl.h>
+#include <string.h>
+#include <cutils/properties.h>
+#include <cutils/log.h>
+
+#define SAMSUNG_BDADDR_PATH "/efs/imei/bt.txt"
+#define BDADDR_PATH "/efs/imei/btaddr.mac"
+#define SUPPL_MACPROP "persist.service.brcm.bt.mac"
+
+int main(void) {
     char tmpbdaddr[23]; // bt_macaddr:xxxxxxxxxxxx
     char bdaddr[18];
     int count;
@@ -27,28 +29,48 @@ int main() {
     }
 
     count = read(fd, tmpbdaddr, sizeof(tmpbdaddr));
+    close(fd);
+
     if (count < 0) {
         fprintf(stderr, "read(%s) failed\n", SAMSUNG_BDADDR_PATH);
         LOGE("Can't read %s\n", SAMSUNG_BDADDR_PATH);
         return -1;
     }
-    else if (count != sizeof(tmpbdaddr)) {
+
+    if (strlen(tmpbdaddr) == 17) {
+
+        fprintf(stderr, "%s has already the good size %d\n", SAMSUNG_BDADDR_PATH, count);
+        LOGD("%s has already the good size %d)\n", SAMSUNG_BDADDR_PATH, count);
+#ifdef SUPPL_MACPROP
+        property_set(SUPPL_MACPROP, tmpbdaddr);
+#endif
+        return 0;
+
+    } else if (count != sizeof(tmpbdaddr)) {
+
         fprintf(stderr, "read(%s) unexpected size %d\n", SAMSUNG_BDADDR_PATH, count);
         LOGE("Error reading %s (unexpected size %d)\n", SAMSUNG_BDADDR_PATH, count);
         return -1;
     }
 
-    count = sprintf(bdaddr, "%2.2s:%2.2s:%2.2s:%2.2s:%2.2s:%2.2s\0",
+    count = sprintf(bdaddr, "%2.2s:%2.2s:%2.2s:%2.2s:%2.2s:%2.2s",
             tmpbdaddr+11,tmpbdaddr+13,tmpbdaddr+15,tmpbdaddr+17,tmpbdaddr+19,tmpbdaddr+21);
 
     fd = open(BDADDR_PATH, O_WRONLY|O_CREAT|O_TRUNC, 0664);
     if (fd < 0) {
         fprintf(stderr, "open(%s) failed\n", BDADDR_PATH);
-        LOGE("Can't open %s\n", BDADDR_PATH);
+        LOGE("Can't open %s for write\n", BDADDR_PATH);
         return -2;
     }
-    write(fd, bdaddr, 18);
+    write(fd, bdaddr, strlen(bdaddr));
     close(fd);
+
+    // hmm... ro
     property_set("ro.bt.bdaddr_path", BDADDR_PATH);
+
+#ifdef SUPPL_MACPROP
+    property_set(SUPPL_MACPROP, tmpbdaddr + sizeof("bt_macaddr"));
+#endif
+
     return 0;
 }
